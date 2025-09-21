82a819fab8b7d7252d18e8f41aca683e1088c696
amol jadhav <amoljadhav@iisc.ac.in>
2025-09-21 11:11:05 +0530

    assignment git repo created

---
+++ Question1/Subquestion1/Question1_A.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define SIZE 2048
+void allocate_matrix(int ***ptr)
+{
+    *ptr = (int**)malloc(SIZE * sizeof(int *));
+     for(int i = 0 ; i <SIZE ; i++)
+     {
+        (*ptr)[i] = (int*)malloc(SIZE * sizeof(int));
+     }
+    
+}
+void initiate_matrix(int **matrix , int flag)
+{
+    for(int i = 0 ; i < SIZE ; i++)
+    {
+        for(int j = 0 ; j < SIZE ; j++)
+        {
+            if(flag==0)
+            matrix[i][j] = rand() % 10;
+            else if(flag = 2)
+            {
+                if(i == j)
+                    matrix[i][j] = 1;
+                else
+                    matrix[i][j] = 0;
+            }
+            else 
+                matrix[i][j] = 0;
+        }
+    }
+}
+void multiplication_order1(int **A,int **B,int **C)
+{
+    for(int i = 0 ; i < SIZE ; i++)
+    {
+        for(int j = 0 ; j < SIZE ; j++)
+        {
+            for(int k = 0 ; k < SIZE ; k++)
+            {
+                C[i][j] += A[i][k]*B[k][j];
+            }
+        }
+    }
+}
+void multiplication_order2(int **A, int **B, int **C)
+{
+    for(int k = 0 ; k < SIZE ; k++)
+    {
+        for(int i = 0 ; i < SIZE ; i++)
+        {
+            for(int j = 0 ; j < SIZE ; j++)
+            {
+                C[i][j] += A[i][k]*B[k][j];
+            }
+        }
+    }
+}
+int main()
+{
+    int **A, **B, **C;
+    allocate_matrix(&A);
+    allocate_matrix(&B);
+    allocate_matrix(&C);
+
+    initiate_matrix(A,1);
+    initiate_matrix(B,1);
+    initiate_matrix(C,0);
+
+
+
+
+
+}
